bf9019ef8144869f8499886857c128143a0a4dde
cryptical-coder-1 <kirambawycliff@gmail.com>
2024-08-08 13:30:29 +0300

    app.js,app.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../App";
-
 import '@testing-library/jest-dom';
 
-// Code tests here
+// Test the initial state of the page
+test("pizza checkbox is initially unchecked", () => {
+  render(<App />);
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  expect(addPepperoni).not.toBeChecked();
+});
+
+test("toppings list initially contains only cheese", () => {
+  render(<App />);
+  expect(screen.getAllByRole("listitem").length).toBe(1);
+  expect(screen.getByText("Cheese")).toBeInTheDocument();
+  expect(screen.queryByText("Pepperoni")).not.toBeInTheDocument();
+});
